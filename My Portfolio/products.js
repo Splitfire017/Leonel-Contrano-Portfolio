cd37afc168a312f50599821d2402c70f41b306b7
Splitfire017 <Splitfirecontrano@gmail.com>
2023-05-20 17:38:15 -0800

    first commit

---
+++ My Portfolio/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Product 1",
+    price: 100,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/sample products.jpg",
+  },
+  {
+    id: 1,
+    name: "Product 2",
+    price: 150,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/digital-planner.jpg",
+  },
+  {
+    id: 2,
+    name: "Product 3",
+    price: 200,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/sample products.jpg",
+  },
+  {
+    id: 3,
+    name: "Product 4",
+    price: 200,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/digital-planner.jpg",
+  },
+  {
+    id: 4,
+    name: "Product 5",
+    price: 200,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/sample products.jpg",
+  },
+  {
+    id: 5,
+    name: "Product 6",
+    price: 200,
+    instock: 20,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./main_assets/digital-planner.jpg",
+  },
+];
---
+++ My Portfolio/script.js
@@ -0,0 +1,177 @@
+// Validation
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+// Add to cart
+
+// SELECT ELEMENTS
+const productsEl = document.querySelector(".products");
+const cartItemsEl = document.querySelector(".cart-items");
+const subtotalEl = document.querySelector(".subtotal");
+const totalItemsInCartEl = document.querySelector(".total-items-in-cart");
+const checkOut = document.querySelector(".checkOut");
+const cartEmpty = document.querySelector(".cartEmpty");
+
+console.log(products);
+// RENDER PRODUCTS
+function renderProducts() {
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+    <div class="card rounded-0" style="width: 345px; height: 350px">
+    <img style="width:343px;height: 200px" src="${product.imgSrc}" alt="${product.name}">
+        <div>${product.name}</div>
+        <p>&#8369;${product.price}</p>
+        <div>${product.description}</div>
+        <div class="add-to-cart" onclick="addToCart(${product.id})">
+        <button class ="btn btncolor rounded-0"  data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight">Add to Cart</button>
+        </div>
+     </div>
+     
+        `;
+  });
+}
+renderProducts();
+
+// cart array
+let cart = JSON.parse(localStorage.getItem("CART")) || [];
+updateCart();
+
+const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+// const userData = user.find((data) => data.email === emailAddress.value);
+// const userData = userInfo?.find((data) => {
+//   return data.isLoggedIn === true;
+// });
+let userData = userInfo.find((user) => user.isLoggedIn === true);
+console.log(userData);
+
+// ADD TO CART
+function addToCart(id) {
+  // check if prodcut already exist in cart
+  if (cart.some((item) => item.id === id)) {
+    changeNumberOfUnits("plus", id);
+  } else {
+    const item = products.find((product) => product.id === id);
+
+    // const userEmail = userData.email;
+    cart.push({
+      ...item,
+      numberOfUnits: 1,
+    });
+  }
+}
+
+// update cart
+function updateCart() {
+  renderCartItems();
+  renderSubtotal();
+
+  // save cart to local storage
+  localStorage.setItem("CART", JSON.stringify(cart));
+}
+
+// calculate and render subtotal
+function renderSubtotal() {
+  let totalPrice = 0,
+    totalItems = 0;
+
+  cart.forEach((item) => {
+    totalPrice += item.price * item.numberOfUnits;
+    totalItems += item.numberOfUnits;
+  });
+
+  subtotalEl.innerHTML = `<h5> Subtotal (${totalItems} items): &#8369;${totalPrice.toFixed(
+    2
+  )}</h5> `;
+
+  totalItemsInCartEl.innerHTML = totalItems;
+}
+if (cart.length === 0) {
+  subtotalEl.innerHTML = "";
+  cartEmpty.innerHTML = `<p>YOUR CART IS EMPTY</p>`;
+}
+
+// render cart items
+function renderCartItems() {
+  cartItemsEl.innerHTML = ""; // clear cart element
+  cart.forEach((item) => {
+    cartItemsEl.innerHTML += `
+        <div class="cart-item row mb-3">
+            <div class="item-info col" >
+                <img style="width:200px;height: 150px" src="${item.imgSrc}" alt="${item.name}">
+                
+            </div>
+            <div class = "col">
+            <div class="unit-price mt-2">
+                <h5>${item.name}</h5>
+                <small>&#8369</small>${item.price}
+            </div>
+            <div class="units text-center d-inline-block m-2" style="border: 1px solid; width: 100px">
+                <div class="btn minus d-inline-block" onclick="changeNumberOfUnits('minus', ${item.id})">-</div>
+                <div class="number d-inline-block">${item.numberOfUnits}</div>
+                <div class="btn plus" onclick="changeNumberOfUnits('plus', ${item.id})">+</div>           
+            </div>
+            <div class="item-info d-inline-block" onclick="removeItemFromCart(${item.id})">
+            <button type="button" class="btn btn-link text-black">REMOVE</button>
+            </div>
+            </div>
+        </div>
+        
+        
+      `;
+    cartEmpty.innerHTML = ``;
+    checkOut.innerHTML = `<a class="btn btncolor w-100 text-center text-space rounded-0"  href="checkout.html">CHECKOUT</a>`;
+  });
+}
+
+// remove item from cart
+function removeItemFromCart(id) {
+  cart = cart.filter((item) => item.id !== id);
+  checkOut.innerHTML = ``;
+  cartEmpty.innerHTML = `<p>YOUR CART IS EMPTY</p>`;
+
+  updateCart();
+}
+
+// change number of units for an item
+function changeNumberOfUnits(action, id) {
+  cart = cart.map((item) => {
+    let numberOfUnits = item.numberOfUnits;
+
+    if (item.id === id) {
+      if (action === "minus" && numberOfUnits > 1) {
+        numberOfUnits--;
+      } else if (action === "plus" && numberOfUnits < item.instock) {
+        numberOfUnits++;
+      }
+    }
+
+    return {
+      ...item,
+      numberOfUnits,
+    };
+  });
+
+  updateCart();
+}
